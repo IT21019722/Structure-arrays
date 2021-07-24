6df4da297eacd05aa7705f5cb9e5744de8af96ca
it21019722 <87459125+IT21019722@users.noreply.github.com>
2021-07-24 08:17:05 +0000

    full answer after testing

--- answer.cpp
+++ answer.cpp
@@ -1,38 +1,43 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 //function prototype
 void averageMarks(int mrk1, int mrk2, float &t_avg);
 //structure Marks 
-struct Marks{
+struct Marks{   //consider as field names 
   int studentNo;
-  int garde;
+  int grade;
   int mark1;
   int mark2;
   float avg;
-}
+};
 //get the input from 5 students;
 int main()
 {
-  for(int x=0; x<5; x++)
+  Marks array1[3];  //array number according to the number of records in the table
+  int x;
+  for(x=0; x<3; x++)
   {
+    cout<<"Student "<<x+1<<endl;
     cout<<"Enter the student number: ";
-    cin>>Marks[x].studentNo;
+    cin>>array1[x].studentNo;
     cout<<"Enter the class grade: ";
-    cin>>Marks[x].grade;
+    cin>>array1[x].grade;
     cout<<"Enter subject 1 mark: ";
-    cin>>Marks[x].mark1;
+    cin>>array1[x].mark1;
     cout<<"Enter subject 2 mark: ";
-    cin>>Marks[x].mark2;
+    cin>>array1[x].mark2;
 
     //call the function 
-    averageMarks(Marks[x].mark1, Marks[x].mark2, Marks[x].avg);
+    averageMarks(array1[x].mark1, array1[x].mark2, array1[x].avg);
+  }
 
-    cout<<"Student Num\tGrade\tSubject 1\tSubject 2\tAverage"<<endl;
-    for(int x=0; x<5; x++)
-    {
-      cout<<Marks[x].studentNo<<"\t"<<Marks[x].grade<<"\t"<<,Marks[x].mark1<<"\t"<<Marks[x].mark2<<endl;
-    }
-
+  cout<<"Student Num\tGrade\tSubject 1\tSubject 2\tAverage"<<endl;  //table headings
+    
+  for(x=0; x<3; x++)//display the table
+  {
+    cout<<array1[x].studentNo<<"\t\t\t"<<array1[x].grade<<"\t\t"<<array1[x].mark1<<"\t\t\t"<<array1[x].mark2<<"\t\t\t";
+    cout<<setw(5)<<setiosflags(ios::fixed)<<setprecision(2)<<array1[x].avg<<endl;
   }
   return 0;
 }
